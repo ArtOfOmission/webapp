8adf37c75b50ec00acc2862df7c8f945b31772f1
chenyansen <huisen0705@live.com>
2019-10-05 14:47:01 +0800

    路由

--- webapp1/src/App.vue
+++ webapp1/src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <!-- 使用 router-link 组件来导航. -->
+      <!-- 通过传入 `to` 属性指定链接. -->
+      <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
+      <router-link to="/">Go to Home</router-link>|
+      <router-link to="/hello">Go to Hello</router-link>
+
+    </div>
+
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <router-view></router-view>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+
   }
 }
 </script>
--- webapp1/src/main.js
+++ webapp1/src/main.js
@@ -1,8 +1,19 @@
+/* eslint-disable */
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
+
+router.beforeEach((to, from, next) => {  
+
+  console.log("to:" + to.fullPath);
+  console.log("from:" + from.fullPath); 
+  next();//跳转
+
+});
