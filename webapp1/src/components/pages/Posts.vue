a9874c75f80943023eecf22fe36b22abc26176e0
chenyansen <huisen0705@live.com>
2019-10-07 10:53:21 +0800

    用按钮获取数据

--- webapp1/src/components/pages/Posts.vue
+++ webapp1/src/components/pages/Posts.vue
@@ -1,30 +1,33 @@
 <template>    
     <div class="s-margin"> 
-        <h2>博客页</h2> 
+        <h2>博客页</h2> <button @click="GetData">Get</button>
+        <div>
             <template>
-                <el-table
-                :data="tableData"
-                style="width: 100%">
-                <el-table-column
-                    prop="title"
-                    label="标题"
-                    width="180">
-                </el-table-column>
-                <el-table-column
-                    prop="body"
-                    label="内容"
-                    width="500">
-                </el-table-column>
-                <el-table-column
-                    prop="author"
-                    label="作者">
-                </el-table-column>
-                <el-table-column
-                    prop="updateTime"
-                    label="更新时间">
-                </el-table-column>
-                </el-table>
+            <el-table
+            :data="tableData"
+            style="width: 100%">
+            <el-table-column
+                prop="title"
+                label="标题"
+                width="180">
+            </el-table-column>
+            <el-table-column
+                prop="body"
+                label="内容"
+                width="500">
+            </el-table-column>
+            <el-table-column
+                prop="author"
+                label="作者">
+            </el-table-column>
+            <el-table-column
+                prop="updateTime"
+                label="更新时间">
+            </el-table-column>
+            </el-table>
             </template>
+        </div>
+            
 
     </div>
 </template>
@@ -37,17 +40,17 @@ export default {
           tableData:null
         }
     },
-    mounted(){
-
-        var $this=this;
-        $this.$axios.get('/api/Posts?pageindex=1&pagesize=5')
-        .then(function (response) {
-            $this.tableData=response.data;
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
+    methods:{
+        GetData(){
+            var $this=this;
+            $this.$axios.get('/api/Posts?pageindex=1&pagesize=5')
+            .then(function (response) {
+                $this.tableData=response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        }
     }
 }
 </script>
